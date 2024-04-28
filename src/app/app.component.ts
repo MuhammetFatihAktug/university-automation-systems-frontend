7d1ddebab8e28151f33955dfb65d76dac312726f
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-04-28 21:25:26 +0300

    File structure changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./components/default-items/header/header.component";
-import {FooterComponent} from "./components/default-items/footer/footer.component";
-import {LoginComponent} from "./components/login-section/login/login.component";
+import {HeaderComponent} from "./shared/components/header/header.component";
+import {FooterComponent} from "./shared/components/footer/footer.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
-import {StudentViewComponent} from "./components/info-section/student-ui/student-view/student-view.component";
 
 export const routes: Routes = [
-  {path: '', component: StudentViewComponent},
-  {path: 'student', component: StudentViewComponent},
-
 ];
