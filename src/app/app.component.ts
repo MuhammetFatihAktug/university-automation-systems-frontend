d4074295f4c0cdc5effa41372eb6a34aaa7cd0f3
Muhammet Fatih Aktug <muhammetfatihaktug@gmail.com>
2024-05-01 19:05:10 +0300

    login section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./shared/components/header/header.component";
 import {FooterComponent} from "./shared/components/footer/footer.component";
--- src/app/core/modules/auth/components/login/login-form/login-form.component.ts
+++ src/app/core/modules/auth/components/login/login-form/login-form.component.ts
@@ -1,18 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import {NgxCaptchaModule} from "ngx-captcha";
-import {AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {NgClass} from "@angular/common";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [NgxCaptchaModule, ReactiveFormsModule, NgClass],
+  imports: [NgxCaptchaModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
   siteKey: string;
   showPassword: boolean = false;
+  loginForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.siteKey = "6Lc-98wpAAAAANmQtrLV78NhB9gYYBHj3FShXV9M";
@@ -23,8 +24,21 @@ export class LoginFormComponent {
     this.showPassword = !this.showPassword;
   }
 
-  onSubmit() {
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      studentNumber: ['', [Validators.required]],
+      studentPassword: ['', Validators.required]
+    });
+    console.log(this.loginForm);
+  }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted successfully');
+
+    } else {
+      console.log('Form is invalid');
+    }
   }
 
 }
